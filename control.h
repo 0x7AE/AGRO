6b022c599b541d384d3b6332b8f88c16027d33af
tomithyr <divinertx@gmail.com>
2024-12-12 10:25:06 +0100

    test

---
+++ control.h
@@ -0,0 +1,25 @@
+#ifndef _CONTROL_H_
+#include <avr/io.h>
+
+
+typedef bool (*AGRO_EVENT)(void);
+typedef void (*AGRO_ACTION )(void);
+
+typedef enum AGRO_STATE {
+    MOVINGx,  MOVINGy,  MOVINGz, PICKING, STOPPED
+} 
+
+typedef struct AGRO_EVENTS {
+     AGRO_STATE current;
+     AGRO_EVENT ledOn;
+     AGRO_EVENT switchOff;
+}
+ 
+bool noEvent();
+bool noAction();
+bool isLedOn();
+void turnLedOn();
+
+#endif
+
+
